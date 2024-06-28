21c2da0da15eeb44fd95897b6b9ff265e65e2dc1
BunnyNomics101 <TranenPeter@gmail.com>
2024-06-28 15:15:30 +0200

    Initial Commit

---
+++ starweb1/starweb1App.swift
@@ -0,0 +1,17 @@
+//
+//  starweb1App.swift
+//  starweb1
+//
+//  Created by Peter Tran on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct starweb1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
