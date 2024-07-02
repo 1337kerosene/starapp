dbac0f036520fe860443a5acd52e6f6c0596b99f
BunnyNomics101 <TranenPeter@gmail.com>
2024-07-02 14:38:20 +0200

    Update ChatStarleet.swift
    chat layout

--- starweb1/Core/ChatStarleet.swift
+++ starweb1/Core/ChatStarleet.swift
@@ -11,8 +11,45 @@ struct ChatStarleet: View {
     var body: some View {
         ZStack{
             Color.starleetBlack.ignoresSafeArea()
-            Text("Chat")
-                .foregroundStyle(.white)
+            VStack{
+                ScrollView{
+                    ForEach(0..<10, id: \.self){ num in
+                        VStack{
+                            HStack(spacing: 16){
+                                Image(systemName: "person.fill")
+                                    .font(.system(size:32))
+                                    .padding(8)
+                                    .foregroundStyle(.starleetMain)
+                                    .overlay(RoundedRectangle(
+                                        cornerRadius: 44)
+                                        .stroke(.starleetWhiteTwo, lineWidth: 1)
+                                    )
+                                VStack(alignment:.leading){
+                                    Text("Satoshi Nakamoto")
+                                        .font(.system(size:16, weight: .bold))
+                                        .foregroundStyle(.starleetWhiteOne)
+                                    Text("You have to buy BITCOIN for access")
+                                        .font(.system(size:14))
+                                        .foregroundStyle(.starleetWhiteTwo)
+                                    
+                                }
+                                .foregroundStyle(.starleetWhiteOne)
+                                .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity, alignment: .leading)
+                                Text("22d")
+                                    .font(.system(size:14, weight: .semibold))
+                                    .foregroundStyle(.starleetMain)
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
+                            
+                        }
+                        .padding(.horizontal)
+                        
+                    }
+                    
+                }
+            }
+            
         }
     }
 }
